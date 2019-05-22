830bce8df2446b3a24559a22d34ac1d313dac7b2
sayakaw <syk_hnsr@yahoo.co.jp>
2019-05-22 13:04:12 +0900

    initial comit ajax の公式です。覚えましょう。

---
+++ assets/js/app.js
@@ -0,0 +1,35 @@
+$(function(){
+
+  $('#search-btn').on('click', function(){
+    // 検索ボタンがクリックされたら
+
+
+    //iTunesに検索をしに行く処理(Ajax)を使います。
+    $.ajax({
+      // データを通信するところ ex:iTunesのAPIのサイトに行ってこぴってくる。’エンドポイント’
+      url:'https://itunes.apple.com/search', 
+      type: 'GET', //get 送信か、post送信か
+      dataType: 'jsonp', //検索結果の形式
+      data: {
+
+        term: 'あいみょん',
+        country: 'jp',
+      }
+
+
+    }).done ( (data) => {
+      // 通信成功したとき
+
+      console.log(data);
+
+    }).fail((error) => {
+      // 通信失敗したとき
+      console.log(error);
+
+    })
+
+    
+
+  });
+
+});
